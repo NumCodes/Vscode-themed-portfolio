7d822b0530a4b3367a4d2a7a33c00809ac5a6eb9
Nweze Ugochukwu Matthew <166723978+NumCodes@users.noreply.github.com>
2026-06-14 07:28:16 +0100

    updated data

--- data/projects.ts
+++ data/projects.ts
@@ -8,12 +8,12 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: 'Driwwwle',
+    title: 'An!on Editor',
     description:
-      'Discover creative websites and developers. A portal for you to share your projects.',
+      'Your all-in-one web development playground right in the browser. Its designed for developers who want a seamless environment to craft, preview, and manage their web projects without the hassle of context switching.',
     logo: '/logos/driwwwle.svg',
-    link: 'https://github.com/itsnitinr/driwwwle',
-    slug: 'driwwwle',
+    link: 'https://github.com/NumCodes/Anion-editor',
+    slug: 'An!on',
   },
   {
     title: 'VSCode Portfolio',
@@ -24,19 +24,11 @@ export const projects: Project[] = [
     slug: 'vscode-portfolio',
   },
   {
-    title: 'Subtrackt',
+    title: 'Accessilearn',
     description:
-      'A simple and elegant way to track your subscriptions and save money.',
-    logo: '/logos/subtrackt.svg',
-    link: 'https://github.com/itsnitinr/subtrackt',
-    slug: 'subtrackt',
-  },
-  {
-    title: 'Coolify Deployments',
-    description:
-      'VSCode extension to track and deploy your Coolify applications.',
+      'An education platform designed for everyone, with built-in accessibility features for users with visual, auditory, and motor disabilities.',
     logo: '/logos/coolify.svg',
-    link: 'https://github.com/itsnitinr/coolify-vscode-extension',
-    slug: 'coolify-vscode-extension',
+    link: 'https://github.com/numcodes/accessilearn',
+    slug: 'accessilearn',
   },
 ];
